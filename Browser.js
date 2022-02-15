ea7c560068440726e56e6018317eb677dd6808cf
Radhaiah <94778126+Radhaiah@users.noreply.github.com>
2022-02-15 21:19:35 +0530

    Add files via upload

---
+++ Browser.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function Browser(){
+    return (<div class="card">
+    <img src="https://static.toiimg.com/thumb/msid-89523549,imgsize-1960350,width-800,height-600,resizemode-75/89523549.jpg" class="card-img-top" alt=""></img>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="https://static.toiimg.com/thumb/msid-89523549,imgsize-1960350,width-800,height-600,resizemode-75/89523549.jpg" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>);
+}
+export default Browser;
